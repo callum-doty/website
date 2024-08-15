e927e4b40bce2a81c623f33078d86b411eccfa19
callum-doty <doty.callum9@gmail.com>
2024-08-15 18:06:34 -0500

    commiti

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Header from './components/Header'
 import './styles/App.css';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Experience" element={<Experience />} />
+          <Route path="/Work" element={<Work />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import '../styles/Header.css';
-
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return <nav className="head">
